49776716b6717b97051fc9e9c057bc6b9f022513
Guilherme <Guilhermegarcia487@gmail.com>
2022-09-19 14:41:54 -0300

    Criação do carousel para poderes

--- rpg_app/lib/view/battle_screen.dart
+++ rpg_app/lib/view/battle_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rpg_app/model/carousel_model.dart';
 
 class BattleScreen extends StatefulWidget {
   const BattleScreen({Key? key}) : super(key: key);
@@ -8,13 +9,16 @@ class BattleScreen extends StatefulWidget {
 }
 
 class _BattleScreenState extends State<BattleScreen> {
+  CarouselModel carousel = new CarouselModel();
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Color(0xff323433),
       body: Padding(
-        padding: EdgeInsets.only(top: size.width * .08, left: size.width*.006),
+        padding:
+            EdgeInsets.only(top: size.width * .08, left: size.width * .006),
         child: Column(
           children: [
             Row(
@@ -30,8 +34,8 @@ class _BattleScreenState extends State<BattleScreen> {
                       height: size.width * .1,
                       width: size.width * .5,
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(5),
-                        color: const Color.fromARGB(25, 217, 217, 217)),
+                          borderRadius: BorderRadius.circular(5),
+                          color: const Color.fromARGB(25, 217, 217, 217)),
                       child: Center(
                         child: Text(
                           "NOME DO USÁRIO",
@@ -50,10 +54,10 @@ class _BattleScreenState extends State<BattleScreen> {
                             child: Container(
                               height: size.width * .1,
                               width: size.width * .1,
-                              decoration:
-                                  BoxDecoration(
-                                    borderRadius: BorderRadius.circular(5),
-                                    color: const Color.fromARGB(25, 217, 217, 217)),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5),
+                                  color:
+                                      const Color.fromARGB(25, 217, 217, 217)),
                               child: Center(
                                 child: Text(
                                   "ICON",
@@ -69,11 +73,10 @@ class _BattleScreenState extends State<BattleScreen> {
                             child: Container(
                               height: size.width * .1,
                               width: size.width * .1,
-                              decoration:
-                                  BoxDecoration(
-                                    borderRadius: BorderRadius.circular(5),
-                                    color: const Color.fromARGB(25, 217, 217, 217),
-                                    ),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                color: const Color.fromARGB(25, 217, 217, 217),
+                              ),
                               child: Center(
                                 child: Text(
                                   "ICON",
@@ -89,10 +92,10 @@ class _BattleScreenState extends State<BattleScreen> {
                             child: Container(
                               height: size.width * .1,
                               width: size.width * .1,
-                              decoration:
-                                  BoxDecoration(
-                                    borderRadius: BorderRadius.circular(5),
-                                    color: const Color.fromARGB(25, 217, 217, 217)),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5),
+                                  color:
+                                      const Color.fromARGB(25, 217, 217, 217)),
                               child: Center(
                                 child: Text(
                                   "ICON",
@@ -108,10 +111,10 @@ class _BattleScreenState extends State<BattleScreen> {
                             child: Container(
                               height: size.width * .1,
                               width: size.width * .1,
-                              decoration:
-                                  BoxDecoration(
-                                    borderRadius: BorderRadius.circular(5),
-                                    color: const Color.fromARGB(25, 217, 217, 217)),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5),
+                                  color:
+                                      const Color.fromARGB(25, 217, 217, 217)),
                               child: Center(
                                 child: Text(
                                   "ICON",
@@ -127,10 +130,10 @@ class _BattleScreenState extends State<BattleScreen> {
                             child: Container(
                               height: size.width * .1,
                               width: size.width * .1,
-                              decoration:
-                                  BoxDecoration(
-                                    borderRadius: BorderRadius.circular(5),
-                                    color: const Color.fromARGB(25, 217, 217, 217)),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5),
+                                  color:
+                                      const Color.fromARGB(25, 217, 217, 217)),
                               child: Center(
                                 child: Text(
                                   "ICON",
@@ -163,14 +166,50 @@ class _BattleScreenState extends State<BattleScreen> {
                 )
               ],
             ),
-            Text("Poderes",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: size.width*.09
+            Text(
+              "Poderes",
+              style: TextStyle(color: Colors.white, fontSize: size.width * .09),
             ),
-            ),
+
             // START CARROSEL
-            
+            Container(
+              width: double.infinity,
+              height: 100.0,
+              //color: Colors.amber,
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: carousel.carouselItens.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    if (index == 0) {
+                      return SizedBox(width: size.width * 0.01);
+                    }
+                    return Container(
+                     
+                      margin: EdgeInsets.all(size.width * 0.02),
+                      width: size.width * 0.167,
+                      height: size.height * 0.167,
+                      
+                      decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black45,
+                              offset: Offset(0, 2),
+                              blurRadius: 6.0,
+                            )
+                          ]),
+                      child: CircleAvatar(
+                        child: ClipOval(
+                          child: Image.network(
+                            carousel.carouselItens[index],
+                            height: size.width * 0.167,
+                            width: size.height * 0.167,
+                          ),
+                        ),
+                      ),
+                    );
+                  }),
+            )
             // END CARROSEL
           ],
         ),
