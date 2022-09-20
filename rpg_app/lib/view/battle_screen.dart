24a64249827baa24194fba6e4e3cf530c2019090
Rafael Lucas <rafaelluc2000@gmail.com>
2022-09-20 00:25:18 -0300

    criado slider de vida e mana

--- rpg_app/lib/view/battle_screen.dart
+++ rpg_app/lib/view/battle_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_sliders/sliders.dart';
 
 class BattleScreen extends StatefulWidget {
   const BattleScreen({Key? key}) : super(key: key);
@@ -8,13 +9,18 @@ class BattleScreen extends StatefulWidget {
 }
 
 class _BattleScreenState extends State<BattleScreen> {
+  int _valueVida = 20;
+  int _valueMana = 20;
+  int maxVida = 100;
+  int maxMana = 100;
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Color(0xff323433),
       body: Padding(
-        padding: EdgeInsets.only(top: size.width * .08, left: size.width*.006),
+        padding:
+            EdgeInsets.only(top: size.width * .08, left: size.width * .006),
         child: Column(
           children: [
             Row(
@@ -30,8 +36,8 @@ class _BattleScreenState extends State<BattleScreen> {
                       height: size.width * .1,
                       width: size.width * .5,
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(5),
-                        color: const Color.fromARGB(25, 217, 217, 217)),
+                          borderRadius: BorderRadius.circular(5),
+                          color: const Color.fromARGB(25, 217, 217, 217)),
                       child: Center(
                         child: Text(
                           "NOME DO USÁRIO",
@@ -50,10 +56,10 @@ class _BattleScreenState extends State<BattleScreen> {
                             child: Container(
                               height: size.width * .1,
                               width: size.width * .1,
-                              decoration:
-                                  BoxDecoration(
-                                    borderRadius: BorderRadius.circular(5),
-                                    color: const Color.fromARGB(25, 217, 217, 217)),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5),
+                                  color:
+                                      const Color.fromARGB(25, 217, 217, 217)),
                               child: Center(
                                 child: Text(
                                   "ICON",
@@ -69,11 +75,10 @@ class _BattleScreenState extends State<BattleScreen> {
                             child: Container(
                               height: size.width * .1,
                               width: size.width * .1,
-                              decoration:
-                                  BoxDecoration(
-                                    borderRadius: BorderRadius.circular(5),
-                                    color: const Color.fromARGB(25, 217, 217, 217),
-                                    ),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                color: const Color.fromARGB(25, 217, 217, 217),
+                              ),
                               child: Center(
                                 child: Text(
                                   "ICON",
@@ -89,10 +94,10 @@ class _BattleScreenState extends State<BattleScreen> {
                             child: Container(
                               height: size.width * .1,
                               width: size.width * .1,
-                              decoration:
-                                  BoxDecoration(
-                                    borderRadius: BorderRadius.circular(5),
-                                    color: const Color.fromARGB(25, 217, 217, 217)),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5),
+                                  color:
+                                      const Color.fromARGB(25, 217, 217, 217)),
                               child: Center(
                                 child: Text(
                                   "ICON",
@@ -108,10 +113,10 @@ class _BattleScreenState extends State<BattleScreen> {
                             child: Container(
                               height: size.width * .1,
                               width: size.width * .1,
-                              decoration:
-                                  BoxDecoration(
-                                    borderRadius: BorderRadius.circular(5),
-                                    color: const Color.fromARGB(25, 217, 217, 217)),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5),
+                                  color:
+                                      const Color.fromARGB(25, 217, 217, 217)),
                               child: Center(
                                 child: Text(
                                   "ICON",
@@ -127,10 +132,10 @@ class _BattleScreenState extends State<BattleScreen> {
                             child: Container(
                               height: size.width * .1,
                               width: size.width * .1,
-                              decoration:
-                                  BoxDecoration(
-                                    borderRadius: BorderRadius.circular(5),
-                                    color: const Color.fromARGB(25, 217, 217, 217)),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5),
+                                  color:
+                                      const Color.fromARGB(25, 217, 217, 217)),
                               child: Center(
                                 child: Text(
                                   "ICON",
@@ -163,15 +168,139 @@ class _BattleScreenState extends State<BattleScreen> {
                 )
               ],
             ),
-            Text("Poderes",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: size.width*.09
-            ),
+            Text(
+              "Poderes",
+              style: TextStyle(color: Colors.white, fontSize: size.width * .09),
             ),
             // START CARROSEL
-            
+
             // END CARROSEL
+            // START SLIDER VIDA
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.heart_broken),
+                            Text("Vida"),
+                            Text("$_valueVida/$maxVida"),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    _valueVida--;
+                                    print(_valueVida);
+                                  });
+                                },
+                                child: Icon(Icons.remove)),
+                            SfSlider(
+                              activeColor: Colors.red,
+                              min: 0.0,
+                              max: maxVida,
+                              value: _valueVida,
+                              showTicks: false,
+                              showLabels: false,
+                              enableTooltip: true,
+                              minorTicksPerInterval: 1,
+                              onChanged: (dynamic value) {
+                                setState(() {
+                                  _valueVida = value;
+                                });
+                              },
+                              thumbIcon: Icon(
+                                Icons.health_and_safety_outlined,
+                                size: 20,
+                              ),
+                              showDividers: true,
+                              stepSize: 1,
+                            ),
+                            GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    _valueVida++;
+                                    print(_valueVida);
+                                  });
+                                },
+                                child: Icon(Icons.add))
+                          ],
+                        )
+                      ],
+                    ),
+                  ],
+                )
+              ],
+            ),
+            // END SLIDER VIDA
+            // START SLIDER MANA
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.hourglass_bottom),
+                            Text("Mana"),
+                            Text("$_valueMana/$maxMana"),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    _valueMana--;
+                                    print(_valueMana);
+                                  });
+                                },
+                                child: Icon(Icons.remove)),
+                            SfSlider(
+                              activeColor: Colors.blue,
+                              min: 0.0,
+                              max: maxMana,
+                              value: _valueMana,
+                              showTicks: false,
+                              showLabels: false,
+                              enableTooltip: true,
+                              minorTicksPerInterval: 1,
+                              onChanged: (dynamic value) {
+                                setState(() {
+                                  _valueMana = value;
+                                });
+                              },
+                              thumbIcon: Icon(
+                                Icons.health_and_safety_outlined,
+                                size: 20,
+                              ),
+                              showDividers: true,
+                              stepSize: 1,
+                            ),
+                            GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    _valueMana++;
+                                    print(_valueMana);
+                                  });
+                                },
+                                child: Icon(Icons.add))
+                          ],
+                        )
+                      ],
+                    ),
+                  ],
+                )
+              ],
+            ),
+            // END SLIDER MANA
           ],
         ),
       ),
