64130f20ccbf6e5c3be7d9452a22ad5351129446
Guilherme <Guilhermegarcia487@gmail.com>
2022-11-05 18:21:46 -0300

    criação da tela de iventario

--- rpg_app/lib/view/invetory_screen.dart
+++ rpg_app/lib/view/invetory_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:rpg_app/style/colors.dart';
+import 'package:rpg_app/model/carousel_model.dart';
+import 'package:rpg_app/view/modal_Item.dart';
 
 class Invetory extends StatefulWidget {
   const Invetory({Key? key}) : super(key: key);
@@ -12,23 +14,105 @@ class _InvetoryState extends State<Invetory> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+    CarouselModel carousel = CarouselModel();
     return Scaffold(
       body: Stack(
         children: [
           Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: size.height*0.9,
-              width: size.width*0.9,
-              color: otherColor,
+            padding: EdgeInsets.only(
+                top: size.width * 0.34, left: size.width * 0.05),
+            child: SizedBox(
+              height: size.height * 0.8,
+              width: size.width * 0.9,
+              child: Card(
+                child: ListView.builder(
+                    itemCount: carousel.carouselItens.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      if (index == 0) {
+                        return SizedBox(
+                          width: size.width * 0.05,
+                        );
+                      }
+                      return SizedBox(
+                        height: size.height * 0.18,
+                        width: size.height * 0.3,
+                        child: Card(
+                            margin: EdgeInsets.all(size.width * 0.04),
+                            child: Row(
+                              children: [
+                                InkWell(
+                                  onTap: index == 1
+                                      ? () => Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: ((context) =>
+                                                  const Invetory())))
+                                      : () => Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: ((context) =>
+                                                  const Invetory()))),
+                                  child: Image.network(
+                                    carousel.carouselItens[index],
+                                    height: size.width * 0.182,
+                                    width: size.height * 0.182,
+                                  ),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.only(top: size.width * 0.02),
+                                  child: Column(
+                                    children: [
+                                      SizedBox(
+                                          width: size.width * 0.42,
+                                          child: Text("Nome: Guilherme")),
+                                      SizedBox(
+                                          width: size.width * 0.42,
+                                          child: Text("Peso: Guilherme")),
+                                      SizedBox(
+                                          width: size.width * 0.42,
+                                          child: Text("Dano: Guilherme")),
+                                      SizedBox(
+                                          width: size.width * 0.42,
+                                          child: Text("Alcance: Guilherme")),
+                                      SizedBox(
+                                          width: size.width * 0.42,
+                                          child: Text("Descrição: Guilherme")),
+                                    ],
+                                  ),
+                                ), 
+                                
+                              ],
+                            )),
+                      );
+                    }),
+                color: defaultColor,
+              ),
             ),
           ),
-         const Text('inventario',
-              style: TextStyle(
-                color: defaultColor,
-                fontWeight: FontWeight.bold,
-                fontSize: 20)),
-
+          const Padding(
+            padding: EdgeInsets.only(top: 90, left: 25),
+            child: Text('Inventario',
+                style: TextStyle(
+                    color: defaultColor,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20)),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+                top: size.width * 0.23, left: size.width * 0.85),
+            child: GestureDetector(
+                onTap: () {
+                     Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: ((context) => const ModalItem())));
+                },
+                child: Icon(
+                  Icons.add,
+                  size: size.height*0.05,
+                  color: secondColor,
+                )),
+          )
         ],
       ),
     );
