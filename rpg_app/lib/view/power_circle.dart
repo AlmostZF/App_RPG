fab591675dfa32e1e340c4780cc5690c694ac348
leticiatakenaka <leticiatakenaka12@gmail.com>
2022-11-16 10:25:39 -0300

    Icone de adicionar

--- rpg_app/lib/view/power_circle.dart
+++ rpg_app/lib/view/power_circle.dart
@@ -16,19 +16,24 @@ class PowerCircle extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
 
     return InkWell(
-      onTap: () {
-        power.id == "1"
-            ? Navigator.of(context).pushNamed(AppRoutes.modalForm)
-            : Navigator.of(context).pushNamed(
-                AppRoutes.modalForm,
-                arguments: power,
-              );
-      },
-      child: CircleAvatar(
-        child: ClipOval(
-          child: Text("${power.nome}"),
-        ),
-      ),
-    );
+        onTap: () {
+          power.id == "1"
+              ? Navigator.of(context).pushNamed(AppRoutes.modalForm)
+              : Navigator.of(context).pushNamed(
+                  AppRoutes.modalForm,
+                  arguments: power,
+                );
+        },
+        child: power.id == "1"
+            ? CircleAvatar(
+                child: ClipOval(
+                  child: Icon(Icons.add),
+                ),
+              )
+            : CircleAvatar(
+                child: ClipOval(
+                  child: Text("${power.nome}"),
+                ),
+              ));
   }
 }
